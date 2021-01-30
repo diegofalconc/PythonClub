c5bcffe6d411bdea9d50db1333578b062102b228
diegofalconc <diego.falconcostilla@seattlecolleges.edu>
2021-01-29 19:58:55 -0600

    product view

--- club/views.py
+++ club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def products(request):
+    product_list = Product.objects.all()
+    return render(request, 'club/products.html', {'product_list': product_list})
